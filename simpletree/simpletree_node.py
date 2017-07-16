3d1006c7ddae57b91ecd610da980c4bd96b10d63
Sylvie Chen <167217@gmail.com>
2017-07-16 20:57:01 +0800

    scene graph model

--- simpletree/simpletree_node.py
+++ simpletree/simpletree_node.py
@@ -11,11 +11,32 @@ class TreeNode(object):
 
         if parent is not None:
             parent.add_child(self)
+
     def name(self):
         return self._name
 
+    def set_name(self, new_name):
+        self._name = new_name
+
+    def type_info(self):
+        return 'node'
+
     def add_child(self, child):
         self._children.append(child)
+
+    def insert_child(self, position, child):
+        if position <0 or position>self.child_count():
+            return False
+        self._children.insert(position, child)
+        child._parent = self
+        return True
+
+    def remove_child(self, position):
+        if position <0 or position>self.child_count():
+            return False
+        child = self._children.pop(position)
+        child._parent = None
+        return True
 
     def child(self, index):
         return self._children[index]
@@ -59,22 +80,37 @@ class SceneGraphModel(QtCore.QAbstractItemModel):
         return parent_node.child_count()
 
     def columnCount(self, parent=QtCore.QModelIndex):
-        return 1
+        return 2
 
     def data(self, index, role):
-        if role == QtCore.Qt.DisplayRole:
-            return index.internalPointer().name()
+        if role == QtCore.Qt.DisplayRole or role == QtCore.Qt.EditRole:
+            if index.column() == 0:
+                return index.internalPointer().name()
+            else:
+                return index.internalPointer().type_info()
 
     def flags(self, index):
-        return QtCore.Qt.ItemIsEnabled | QtCore.Qt.ItemIsSelectable
+        return QtCore.Qt.ItemIsEnabled | QtCore.Qt.ItemIsSelectable | QtCore.Qt.ItemIsEditable
+
+    def setData(self, index, value, role=QtCore.Qt.EditRole):
+        if index.isValid():
+            if role == QtCore.Qt.EditRole:
+                node = index.internalPointer()
+                node.set_name(value)
+                return True
 
     def headerData(self, section, orientation, role):
-        return 'Scenegraph'
+        if role == QtCore.Qt.DisplayRole:
+            if section == 0:
+                return 'Scenegraph'
+            else:
+                return 'TYPE'
 
     def parent(self, index):
         # Returns a void * pointer used by the model to associate the index with the internal data structure.
-        node = index.internalPointer()
+        node = self.get_node(index)
         parent_node = node.parent()
+
 
         if parent_node == self._rootnode:
             return QtCore.QModelIndex()
@@ -84,12 +120,7 @@ class SceneGraphModel(QtCore.QAbstractItemModel):
         return self.createIndex(parent_node.index(), 0, parent_node)
 
     def index(self, row, column, parent):
-        if not parent.isValid():
-            parent_node = self._rootnode
-        else:
-            parent_node = parent.internalPointer()
-
-        #
+        parent_node = self.get_node(parent)
         children_item = parent_node.child(row)
 
         if children_item:
@@ -97,21 +128,76 @@ class SceneGraphModel(QtCore.QAbstractItemModel):
         else:
             return QtCore.QModelIndex()
 
+    def insertRows(self, row, count, parent=QtCore.QModelIndex()):
+        self.beginInsertRows(parent, row, row+count-1)
+        parent_node = self.get_node(parent)
+        for i in range(count):
+            node_name = 'untitled{}'.format(parent_node.child_count())
+            child_node = TreeNode(node_name)
+            success = parent_node.insert_child(row, child_node)
+        self.endInsertRows()
+        return success
+
+    def removeRows(self, row, count, parent=QtCore.QModelIndex()):
+        self.beginRemoveRows(parent, row, row+count-1)
+        node = self.get_node(parent)
+        for i in range(count):
+            success = node.remove_child(row)
+        self.endRemoveRows()
+        return success
+
+    # ====================================== #
+    #   customize
+    # ====================================== #
+    def get_node(self, index):
+        if index.isValid():
+            node = index.internalPointer()
+            if node:
+                return node
+
+        return self._rootnode
+
+class CameraTreeNode(TreeNode):
+    def __init__(self, name, parent=None):
+        super(CameraTreeNode,  self).__init__(name, parent)
+
+    def type_info(self):
+        return 'camera'
+
+
+class GeometryTreeNode(TreeNode):
+    def __init__(self, name, parent=None):
+        super(GeometryTreeNode,  self).__init__(name, parent)
+
+    def type_info(self):
+        return 'Geo'
+
 if __name__ == '__main__':
     import sys
     app = QtGui.QApplication(sys.argv)
+    app.setStyle('playstique')
 
-    rootnode = TreeNode('drinks')
-    children_01 = TreeNode('coffee', rootnode)
-    children_02 = TreeNode('tea', rootnode)
-    children_03 = TreeNode('milk_tea', children_02)
+    rootnode = TreeNode('root')
+
+    camera_node = CameraTreeNode('Cam', rootnode)
+    geo_node = GeometryTreeNode('body', rootnode)
+    sphere_node = GeometryTreeNode('leg', geo_node)
 
     print rootnode
 
-    scene_model = SceneGraphModel(rootnode)
     tree_view = QtGui.QTreeView()
     tree_view.show()
 
+    scene_model = SceneGraphModel(rootnode)
     tree_view.setModel(scene_model)
+
+    # insert/remove node as child of root node
+    scene_model.insertRows(0, 3)
+    scene_model.removeRows(0, 1)
+
+    # insert node as child of body node
+    body_index = scene_model.index(3, 0, QtCore.QModelIndex())
+    scene_model.insertRows(0, 3, body_index)
+
 
     sys.exit(app.exec_())
